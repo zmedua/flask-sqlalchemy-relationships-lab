24aa17e28d675c3e29201fca5cfbe263b4c349f4
Zane <zimedua@gmail.com>
2026-06-18 17:09:46 -0500

    lab flask sql many - many relationships

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 
 from models import db, Event, Session, Speaker, Bio
@@ -18,28 +18,80 @@ db.init_app(app)
 
 @app.route('/events')
 def get_events():
-    pass
+    events = Event.query.all()
+
+    body = [
+        {
+            "id": event.id,
+            "name": event.name,
+            "location": event.location
+        }
+        for event in events
+    ]
+    return make_response(body, 200)
 
 
 @app.route('/events/<int:id>/sessions')
 def get_event_sessions(id):
-    pass
+    event = Event.query.filter_by(id=id).first()
 
+    if not event:
+        return make_response({"error": "Event not found"}, 404)
 
+    body = [
+        {
+            "id": session.id,
+            "title": session.title,
+            "start_time": session.start_time.isoformat()
+        }
+        for session in event.sessions
+    ]
+    return make_response(body, 200)
 @app.route('/speakers')
 def get_speakers():
-    pass
+    speakers = Speaker.query.all()
 
+    body = [
+        {
+        "id": speaker.id,
+        "name": speaker.name
+        }
+        for speaker in speakers
+    ]
+    return make_response(body, 200)
 
 @app.route('/speakers/<int:id>')
 def get_speaker(id):
-    pass
+    speaker = Speaker.query.filter_by(id=id).first()
+
+    if not speaker:
+        return make_response({"error": "Speaker not found"}, 404)
+    
+    body = {
+        "id": speaker.id,
+        "name": speaker.name,
+        "bio_text": speaker.bio.bio_text if speaker.bio else "No bio available"
+    }
+
+    return make_response(body, 200)
 
 
 @app.route('/sessions/<int:id>/speakers')
 def get_session_speakers(id):
-    pass
+    session = Session.query.filter_by(id=id).first()
 
+    if not session:
+        return make_response({"error": "Session not found"}, 404)
+    
+    body = [
+        {
+            "id": speaker.id,
+            "name": speaker.name,
+            "bio_text": speaker.bio.bio_text if speaker.bio else "No bio available"
+        }
+        for speaker in session.speakers
+    ]
+    return make_response(body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -9,7 +9,11 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # TODO: add association table
-
+session_speakers = db.Table(
+    "session_speakers",
+    db.Column("session_id", db.Integer, db.ForeignKey("sessions.id"), primary_key=True),
+    db.Column("speaker_id", db.Integer, db.ForeignKey("speakers.id"), primary_key=True)
+)
 
 # TODO: set up relationships for all models
 class Event(db.Model):
@@ -18,7 +22,12 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     location = db.Column(db.String, nullable=False)
-
+    
+    sessions = db.relationship(
+        "Session",
+        back_populates="event",
+        cascade="all, delete-orphan"
+    )
     def __repr__(self):
         return f'<Event {self.id}, {self.name}, {self.location}>'
 
@@ -28,8 +37,14 @@ class Session(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     start_time = db.Column(db.DateTime)
-    event_id = db.Column(db.Integer)
+    event_id = db.Column(db.Integer, db.ForeignKey("events.id"))
 
+    event = db.relationship("Event", back_populates="sessions")
+    speakers = db.relationship(
+        "Speaker",
+        secondary=session_speakers,
+        back_populates="sessions"
+    )
 
     def __repr__(self):
         return f'<Session {self.id}, {self.title}, {self.start_time}>'
@@ -41,15 +56,28 @@ class Speaker(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
 
+    bio = db.relationship(
+        "Bio",
+        back_populates='speaker',
+        cascade="all, delete-orphan",
+        uselist=False
+    )
+
+    sessions = db.relationship(
+        "Session",
+        secondary=session_speakers,
+        back_populates="speakers"
+    )
     def __repr__(self):
-        return f'<Speaker {id}, {name}>'
+        return f'<Speaker {self.id}, {self.name}>'
 
 class Bio(db.Model):
     __tablename__ = 'bios'
 
     id = db.Column(db.Integer, primary_key=True)
     bio_text = db.Column(db.Text, nullable=False)
-    speaker_id = db.Column(db.Integer)
+    speaker_id = db.Column(db.Integer, db.ForeignKey("speakers.id"))
 
+    speaker = db.relationship("Speaker", back_populates="bio")
     def __repr__(self):
-        return f'<Bio {id}, {bio_text}>'
+        return f'<Bio {self.id}, {self.bio_text}>'
